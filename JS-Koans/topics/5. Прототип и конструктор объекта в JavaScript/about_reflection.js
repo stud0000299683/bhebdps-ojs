65a69004d1fe908cf98e0fda97a5a6000cbce033
stud0000299683 <145689661+stud0000299683@users.noreply.github.com>
2024-09-29 16:13:32 +0300

    Update about_reflection.js

--- JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_reflection.js
+++ JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_reflection.js
@@ -17,9 +17,9 @@ describe("About Reflection (about_reflection.js)", function() {
       keys.push(propertyName);
     }
     // how many elements are in the keys array?
-    expect(FILL_ME_IN).toBe(keys.length);
+    expect(2).toBe(keys.length);
     // what are the properties of the array?
-    expect([FILL_ME_IN, FILL_ME_IN]).toEqual(keys);
+    expect(['bprop', 'aprop']).toEqual(keys);
 
     // hasOwnProperty returns true if the parameter is a property directly on the object,
     // but not if it is a property accessible via the prototype chain.
@@ -31,19 +31,19 @@ describe("About Reflection (about_reflection.js)", function() {
     }
 
     // how many elements are in the ownKeys array?
-    expect(FILL_ME_IN).toBe(ownKeys.length);
+    expect(1).toBe(ownKeys.length);
     // what are the own properties of the array?
-    expect([FILL_ME_IN]).toEqual(ownKeys);
+    expect(['bprop']).toEqual(ownKeys);
   });
 
   it("constructor property", function () {
     let a = new A();
     let b = new B();
     // "what is the type of a's constructor?"
-    expect(FILL_ME_IN).toBe(typeof(a.constructor));
+    expect('function').toBe(typeof(a.constructor));
     // "what is the name of a's constructor?"
-    expect(FILL_ME_IN).toBe(a.constructor.name);
+    expect('A').toBe(a.constructor.name);
     // "what is the name of b's constructor?"
-    expect(FILL_ME_IN).toBe(b.constructor.name);
+    expect('A').toBe(b.constructor.name);
   });
 });
