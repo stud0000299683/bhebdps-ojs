0a00683577434ca590ed9afa2831806ec623c7a3
stud0000299683 <145689661+stud0000299683@users.noreply.github.com>
2024-09-29 16:13:09 +0300

    Update about_prototype_chain.js

--- JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_prototype_chain.js
+++ JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_prototype_chain.js
@@ -25,35 +25,35 @@ describe("About Prototype Chain (about_prototype_chain.js)", function() {
   
   it("Is there an 'a' and 'b' own property on child?", function () {
     // child.hasOwnProperty(\'a\')?
-    expect(FILL_ME_IN).toBe(child.hasOwnProperty('a'));
+    expect(true).toBe(child.hasOwnProperty('a'));
     // child.hasOwnProperty(\'b\')?
-    expect(FILL_ME_IN).toBe(child.hasOwnProperty('b'));
+    expect(true).toBe(child.hasOwnProperty('b'));
   });
   
   it("Is there an 'a' and 'b' property on child?", function () {
     // what is \'a\' value?
-    expect(FILL_ME_IN).toBe(child.a);
+    expect(1).toBe(child.a);
     // what is \'b\' value?
-    expect(FILL_ME_IN).toBe(child.b);
+    expect(2).toBe(child.b);
   });
   
   it("If 'b' was removed, whats b value?", function () {
     delete child.b;
     // what is \'b\' value now?
-    expect(FILL_ME_IN).toBe(child.b);
+    expect(3).toBe(child.b);
   });
   
   
   it("Is there a 'c' own property on child?", function () {
     // child.hasOwnProperty(\'c\')?
-    expect(FILL_ME_IN).toBe(child.hasOwnProperty('c'));
+    expect(false).toBe(child.hasOwnProperty('c'));
   });
   
   // Is there a 'c' own property on child? No, check its prototype
   // Is there a 'c' own property on child.[[Prototype]]? Yes, its value is...
   it("Is there a 'c' property on child?", function () {
     // what is the value of child.c?
-    expect(FILL_ME_IN).toBe(child.c);
+    expect(4).toBe(child.c);
   });
   
   
@@ -62,6 +62,6 @@ describe("About Prototype Chain (about_prototype_chain.js)", function() {
   // child.[[Prototype]].[[Prototype]] is null, stop searching, no property found, return...
   it("Is there an 'd' property on child?", function () {
     // what is the value of child.d?
-    expect(FILL_ME_IN).toBe(child.d);
+    expect(undefined).toBe(child.d);
   });
 });
